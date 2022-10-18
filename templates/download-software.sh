71b61a35bbe1da3ff16506824fdedffbfb7b649a
Jack Morrice <jackmo375@gmail.com>
2022-10-18 13:51:40 +0200

    updating config files

--- templates/download-software.sh
+++ templates/download-software.sh
@@ -10,6 +10,13 @@ wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.9.zi
 unzip -n -d ${software_dir} "${software_dir}/fastqc_v0.11.9.zip"
 chmod +x $fastqc
 
+# trim galore
+wget https://github.com/FelixKrueger/TrimGalore/archive/refs/tags/0.6.7.zip \
+    --directory-prefix $software_dir \
+    --no-clobber
+unzip -n -d ${software_dir} "${software_dir}/0.6.7.zip"
+
+
 # bwa
 wget https://github.com/lh3/bwa/releases/download/v0.7.17/bwa-0.7.17.tar.bz2 \
     --directory-prefix $software_dir \
@@ -23,3 +30,9 @@ wget https://github.com/broadinstitute/gatk/releases/download/4.3.0.0/gatk-4.3.0
     --directory-prefix $software_dir \
     --no-clobber
 unzip -n -d ${software_dir} "${software_dir}/gatk-4.3.0.0.zip"
+
+# qualimap
+wget https://bitbucket.org/kokonech/qualimap/downloads/qualimap_v2.2.1.zip \
+    --directory-prefix $software_dir \
+    --no-clobber
+unzip -n -d ${software_dir} "${software_dir}/qualimap_v2.2.1.zip"
