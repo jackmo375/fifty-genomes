b6f1752a83ce37ca9ef47c2369ee19129ad43f4a
Jack Morrice <jackmo375@gmail.com>
2022-10-19 10:03:52 +0200

    fixed BQSR comparsion bug

--- templates/pre-processing/compare-pre-and-post-base-recalibration-models.sh
+++ templates/pre-processing/compare-pre-and-post-base-recalibration-models.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# note, you need R installed, and packages available:
+# gplots, gsalib
+# On this computer I have them installed for R v4.1
+# so I need to make sure I load this R version.
+#
+# This script takes seconds, so does not need to be
+# qsubbed.
+#
+# Jack Morrice
+# 2022-10-19
+
 source ./config.sh
 
 sample_id="SCD109"
